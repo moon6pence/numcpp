cd1798d3f51d093b1293efe59b2135bd3df2a981
khkim <moon6pence@gmail.com>
2013-11-05 15:13:38 +0900

    Implement 1d array allocator function
    - Remove constructor without origin pointer - We want to keep template array struct as simple as possible

--- numcpp2/array.h
+++ numcpp2/array.h
@@ -12,16 +12,12 @@ struct array_t
 	typedef T value_type;
 
 private:
+	// TODO: make member variables const
 	std::shared_ptr<void> _address;
 	value_type *_origin;
-	shape_type _shape;
+	std::unique_ptr<size_type> _shape;
 
 public:
-	array_t(std::shared_ptr<void> address, shape_type shape) :
-		array_t(address, address, shape)
-	{
-	}
-
 	array_t(std::shared_ptr<void> address, value_type *origin, shape_type shape) :
 		_address(address), 
 		_origin(origin), 
@@ -29,5 +25,30 @@ public:
 	{
 	}
 };
+
+template <typename T>
+void array_deleter(T const *p)
+{
+	delete[] p;
+}
+
+template <typename T>
+array_t<T, 1> array(int length)
+{
+	// allocate buffer
+	T *buffer = new T[length];
+
+	// address
+	std::shared_ptr<void> address(buffer, array_deleter<T>);
+
+	// origin
+	T *origin = buffer;
+
+	// shape
+	size_type *shape = new int[1];
+	shape[0] = length;
+
+	return array_t<T, 1>(address, origin, shape);
+}
 
 #endif // __ARRAY_H__
--- test_main.cpp
+++ test_main.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
+	// Test: Program is just working
 	printf("Hello World!\n");
+
+	// Test: allocate 1d array
+	auto test1 = array<int>(1024);
+
 	return 0;
 }
