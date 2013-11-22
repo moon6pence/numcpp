12ef68a849697cb4a8f6ddf516cb045075a09011
khkim <moon6pence@gmail.com>
2013-11-22 09:35:50 +0900

    Add config.h for conditional build

--- numcpp.h
+++ numcpp.h
@@ -1,8 +1,14 @@
 #ifndef __NUMCPP_H__
 #define __NUMCPP_H__
 
+#include "config.h"
+
 #include "numcpp/array.h"
 #include "numcpp/array_allocate.h"
 #include "numcpp/array_function.h"
+
+#ifdef USE_MAGICK
+#include "numcpp/image.h"
+#endif
 
 #endif // __NUMCPP_H__
