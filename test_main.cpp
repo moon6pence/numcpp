af400acd6e6ae0ed0f14419e95311e5fd1c93cca
khkim <moon6pence@gmail.com>
2013-12-22 21:11:08 +0900

    Make test fixture

--- test_main.cpp
+++ test_main.cpp
@@ -51,23 +51,51 @@ TEST(ArrayType, DeclareArrayWithSize)
 	EXPECT_NE(a3.raw_ptr(), nullptr);
 }
 
-TEST(ArrayType, AccessElements)
+class ArrayTypeF : public ::testing::Test
 {
-	array_t<int> a1(5);
+public:
+	ArrayTypeF() : a1(5), a2(2, 3), a3(2, 3, 4)
+	{
+	}
 
-	a1.at(0) = 2;
-	a1.at(1) = 3;
-	// a1.at(2) = 5;
-	// a1.at(3) = 1;
-	a1(2) = 5;
-	a1(3) = 1;
-	a1.at(4) = 7;
+protected:
+	virtual void SetUp()
+	{
+		memcpy(a1.raw_ptr(), data1, 5 * sizeof(int));
+		memcpy(a2.raw_ptr(), data2, 6 * sizeof(int));
+		memcpy(a3.raw_ptr(), data3, 24 * sizeof(int));
+	}
 
+	virtual void TearDown() {}
+
+	array_t<int> a1, a2, a3;
+
+	const int data1[5] = { 2, 3, 5, 1, 7 };
+
+	const int data2[6] = 
+	{ 
+		7, 2, 3, 
+		4, 1, 8 
+	};
+
+	const int data3[24] = 
+	{
+		32, 19, 22, 10, 
+		81, 42, 71, 86, 
+		44, 66, 77, 88, 
+
+		98, 76, 54, 32, 
+		15, 16, 17, 18, 
+		21, 22, 23, 24
+	};	
+};
+
+TEST_F(ArrayTypeF, AccessElements)
+{
+	// 1d array
 	EXPECT_EQ(a1.at(0), 2);
 	EXPECT_EQ(a1.at(1), 3);
 	EXPECT_EQ(a1.at(2), 5);
-	// EXPECT_EQ(a1.at(3), 1);
-	// EXPECT_EQ(a1.at(4), 7);
 	EXPECT_EQ(a1(3), 1);
 	EXPECT_EQ(a1(4), 7);
 
@@ -82,49 +110,24 @@ TEST(ArrayType, AccessElements)
 	EXPECT_EQ(a1(3), 9);
 	EXPECT_EQ(ptr, ptr2);
 
-	int data2[6] = 
-	{ 
-		7, 2, 3, 
-		4, 1, 8 
-	};
-
-	array_t<int> a2(2, 3);
-	for (int i = 0; i < 6; i++)
-		a2(i) = data2[i];
-
+	// 2d array
 	EXPECT_EQ(a2(0), 7);
 	EXPECT_EQ(a2(2), 3);
 	EXPECT_EQ(a2(5), 8);
 
 	EXPECT_EQ(a2.at(0, 0), 7);
-	// EXPECT_EQ(a2.at(1, 1), 1);
 	EXPECT_EQ(a2(1, 1), 1);
 	EXPECT_EQ(a2.at(1, 2), 8);
 
 	a2.at(0, 2) = 5;
 	EXPECT_EQ(a2.at(0, 2), 5);
 
-	int data3[24] = 
-	{
-		32, 19, 22, 10, 
-		81, 42, 71, 86, 
-		44, 66, 77, 88, 
-
-		98, 76, 54, 32, 
-		15, 16, 17, 18, 
-		21, 22, 23, 24
-	};	
-
-	array_t<int> a3(2, 3, 4);
-	for (int i = 0; i < 24; i++)
-		a3(i) = data3[i];
-
+	// 3d array
 	EXPECT_EQ(a3.at(0), 32);
 	EXPECT_EQ(a3(15), 32);
 	EXPECT_EQ(a3(23), 24);
 
 	EXPECT_EQ(a3.at(0, 0, 0), 32);
-	// EXPECT_EQ(a3.at(0, 1, 2), 71);
 	EXPECT_EQ(a3(0, 1, 2), 71);
 	EXPECT_EQ(a3.at(1, 2, 3), 24);
 
