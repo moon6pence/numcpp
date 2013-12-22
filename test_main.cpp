31ec9241f2c6de22655e877cd1529fdb29f98c97
khkim <moon6pence@gmail.com>
2013-12-22 14:15:37 +0900

    minor

--- test_main.cpp
+++ test_main.cpp
@@ -8,11 +8,11 @@ TEST(Main, HelloGoogleTest)
 
 #include <numcpp/array.h>
 
-namespace np = numcpp;
+using namespace numcpp;
 
 TEST(ArrayType, DeclareEmptyArray)
 {
-	np::array_t<int> a1;
+	array_t<int> a1;
 
 	ASSERT_TRUE(a1.empty());
 	ASSERT_EQ(a1.size(), 0);
@@ -21,7 +21,7 @@ TEST(ArrayType, DeclareEmptyArray)
 
 TEST(ArrayType, DeclareArrayWithSize)
 {
-	np::array_t<int> a1(5);
+	array_t<int> a1(5);
 
 	ASSERT_FALSE(a1.empty());
 	ASSERT_EQ(a1.size(), 5);
