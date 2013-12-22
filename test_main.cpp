1e4126c20aed0246732ea3597c0b144bc7f1a570
khkim <moon6pence@gmail.com>
2013-12-22 14:45:39 +0900

    Use EXPECT rather than ASSERT

--- test_main.cpp
+++ test_main.cpp
@@ -15,8 +15,8 @@ TEST(ArrayType, DeclareEmptyArray)
 	array_t<int> a1;
 
 	ASSERT_TRUE(a1.empty());
-	ASSERT_EQ(a1.size(), 0);
-	ASSERT_EQ(a1.raw_ptr(), nullptr);
+	EXPECT_EQ(a1.size(), 0);
+	EXPECT_EQ(a1.raw_ptr(), nullptr);
 }
 
 TEST(ArrayType, DeclareArrayWithSize)
@@ -24,8 +24,8 @@ TEST(ArrayType, DeclareArrayWithSize)
 	array_t<int> a1(5);
 
 	ASSERT_FALSE(a1.empty());
-	ASSERT_EQ(a1.size(), 5);
-	ASSERT_NE(a1.raw_ptr(), nullptr);
+	EXPECT_EQ(a1.size(), 5);
+	EXPECT_NE(a1.raw_ptr(), nullptr);
 }
 
 TEST(ArrayType, AccessElements)
@@ -40,22 +40,22 @@ TEST(ArrayType, AccessElements)
 	a1(3) = 1;
 	a1.at(4) = 7;
 
-	ASSERT_EQ(a1.at(0), 2);
-	ASSERT_EQ(a1.at(1), 3);
-	ASSERT_EQ(a1.at(2), 5);
-	ASSERT_EQ(a1.at(3), 1);
-	ASSERT_EQ(a1.at(4), 7);
+	EXPECT_EQ(a1.at(0), 2);
+	EXPECT_EQ(a1.at(1), 3);
+	EXPECT_EQ(a1.at(2), 5);
+	EXPECT_EQ(a1.at(3), 1);
+	EXPECT_EQ(a1.at(4), 7);
 
 	int *ptr = a1.raw_ptr();
 	ptr[2] = 8;
 
-	ASSERT_EQ(a1(2), 8);
+	EXPECT_EQ(a1(2), 8);
 
 	int *ptr2 = a1;
 	ptr[3] = 9;
 
-	ASSERT_EQ(a1(3), 9);
-	ASSERT_EQ(ptr, ptr2);
+	EXPECT_EQ(a1(3), 9);
+	EXPECT_EQ(ptr, ptr2);
 }
 
 int main(int argc, char **argv)
