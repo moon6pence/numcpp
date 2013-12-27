3036417eda2a2ed6006e545579e04ad81580315a
khkim <moon6pence@gmail.com>
2013-12-27 23:17:39 +0900

    Conditional build: USE_OPENCV

--- test_main.cpp
+++ test_main.cpp
@@ -6,10 +6,15 @@ TEST(Main, HelloGoogleTest)
 	EXPECT_EQ(1 + 1, 2);
 }
 
+#include "config.h"
+
 #include "numcpp/array.test.inl"
 #include "numcpp/stl.test.inl"
 #include "numcpp/functions.test.inl"
+
+#ifdef USE_OPENCV
 #include "numcpp/opencv.test.inl"
+#endif
 
 int main(int argc, char **argv)
 {
