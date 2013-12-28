66e622482fbcf615d7910375af2e33003216a846
khkim <moon6pence@gmail.com>
2013-12-28 17:56:02 +0900

    Introduce abstract_array_t

--- numcpp/array.h
+++ numcpp/array.h
@@ -6,104 +6,38 @@
 namespace numcpp {
 
 template <typename T>
-void array_deleter(T *ptr)
+struct abstract_array_t
 {
-	delete[] ptr;
-}
-
-template <typename T>
-struct array_t
-{
-private:
+protected:
 	int _ndims;
 	int _size;
 	int *_shape;
 	std::shared_ptr<T> _ptr;
 
-public:
-	array_t() : 
-		_ndims(1), _size(0), _shape(nullptr)
+	void init()
 	{
+		_ndims = 1;
+		_size = 0;
 		_shape = new int[1];
 		_shape[0] = 0;
+		_ptr = nullptr;	
 	}
 
-	array_t(int size0) : 
-		_ndims(1), _size(size0), _shape(nullptr)
+	void init(int ndims, int size, int *shape, std::shared_ptr<T> ptr)
 	{
-		_shape = new int[1];
-		_shape[0] = size0;
-
-		_ptr = std::shared_ptr<T>(new T[_size], array_deleter<T>);
+		_ndims = ndims;
+		_size = size;
+		_shape = shape;
+		_ptr = ptr;
 	}
 
-	array_t(int size0, int size1) : 
-		_ndims(2), _size(size0 * size1), _shape(nullptr)
-	{
-		_shape = new int[2];
-		_shape[0] = size0;
-		_shape[1] = size1;
-
-		_ptr = std::shared_ptr<T>(new T[_size], array_deleter<T>);	
-	}
-
-	array_t(int size0, int size1, int size2) : 
-		_ndims(3), _size(size0 * size1 * size2), _shape(nullptr)
-	{
-		_shape = new int[3];
-		_shape[0] = size0;
-		_shape[1] = size1;
-		_shape[2] = size2;
-
-		_ptr = std::shared_ptr<T>(new T[_size], array_deleter<T>);	
-	}
-
-	~array_t()
+	void free()
 	{
 		if (_shape) { delete[] _shape; _shape = nullptr; }
 		_ptr = nullptr;
 	}
 
-private:
-	// disable copy constructor, assign
-	array_t(array_t &) { }
-	const array_t &operator=(const array_t &) { return *this; }
-
 public:
-	// move constructor
-	array_t(array_t &&other) :
-		_ndims(other._ndims), 
-		_size(other._size), 
-		_shape(other._shape), 
-		_ptr(std::move(other._ptr))
-	{
-		other._ndims = 1;
-		other._size = 0;
-		other._shape = new int[1];
-		other._shape[0] = 0;
-		other._ptr = nullptr;
-	}
-
-	// move assign
-	const array_t &operator=(array_t &&other)
-	{
-		if (_shape) { delete _shape; _shape = nullptr; }
-		_ptr = nullptr;
-
-		_ndims = other._ndims;
-		_size = other._size;
-		_shape = other._shape;
-		_ptr = std::move(other._ptr);
-
-		other._ndims = 1;
-		other._size = 0;
-		other._shape = new int[1];
-		other._shape[0] = 0;
-		other._ptr = nullptr;
-
-		return *this;
-	}
-
 	bool empty() const
 	{
 		return _size == 0;
@@ -206,6 +140,94 @@ public:
 	const T& operator() (int index0, int index1, int index2) const
 	{
 		return at(index0, index1, index2);
+	}
+};
+
+template <typename T>
+void array_deleter(T *ptr)
+{
+	delete[] ptr;
+}
+
+template <typename T>
+struct array_t : public abstract_array_t<T>
+{
+public:
+	array_t()
+	{
+		abstract_array_t<T>::init();
+	}
+
+	array_t(int size0)
+	{
+		int size = size0;
+
+		int *shape = new int[1];
+		shape[0] = size0;
+
+		auto ptr = std::shared_ptr<T>(new T[size], array_deleter<T>);
+
+		abstract_array_t<T>::init(1, size, shape, ptr);
+	}
+
+	array_t(int size0, int size1)
+	{
+		int size = size0 * size1;
+
+		int *shape = new int[2];
+		shape[0] = size0;
+		shape[1] = size1;
+
+		auto ptr = std::shared_ptr<T>(new T[size], array_deleter<T>);	
+
+		abstract_array_t<T>::init(2, size, shape, ptr);
+	}
+
+	array_t(int size0, int size1, int size2)
+	{
+		int size = size0 * size1 * size2;
+
+		int *shape = new int[3];
+		shape[0] = size0;
+		shape[1] = size1;
+		shape[2] = size2;
+
+		auto ptr = std::shared_ptr<T>(new T[size], array_deleter<T>);	
+
+		abstract_array_t<T>::init(3, size, shape, ptr);
+	}
+
+	~array_t()
+	{
+		abstract_array_t<T>::free();
+	}
+
+private:
+	// disable copy constructor, assign
+	array_t(array_t &) { }
+	const array_t &operator=(const array_t &) { return *this; }
+
+public:
+	// move constructor
+	array_t(array_t &&other)
+	{
+		abstract_array_t<T>::init(
+			other._ndims, other._size, other._shape, std::move(other._ptr));
+
+		other.init();
+	}
+
+	// move assign
+	const array_t &operator=(array_t &&other)
+	{
+		abstract_array_t<T>::free();
+
+		abstract_array_t<T>::init(
+			other._ndims, other._size, other._shape, std::move(other._ptr));
+
+		other.init();
+
+		return *this;
 	}
 };
 
