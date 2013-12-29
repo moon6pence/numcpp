3ef34895722aa2af4d6b5a6fb8c2378ec8641e00
khkim <moon6pence@gmail.com>
2013-12-30 00:39:49 +0900

    Add function to_cv_gpu_mat(device_array_t)

--- numcpp/opencv.h
+++ numcpp/opencv.h
@@ -1,8 +1,15 @@
 #ifndef NUMCPP_OPENCV_H_
 #define NUMCPP_OPENCV_H_
 
+#include "../config.h"
+
 #include <opencv/cv.h>
 #include <opencv2/highgui/highgui.hpp>
+
+#ifdef USE_CUDA
+#include "cuda.h"
+#include <opencv2/gpu/gpu.hpp>
+#endif
 
 namespace numcpp {
 
@@ -31,6 +38,22 @@ array_t<T> from_cv_mat(const cv::Mat &cv_mat)
 
 	return std::move(result);
 }
+
+#ifdef USE_CUDA
+
+template <typename T>
+inline cv::gpu::GpuMat to_cv_gpu_mat(device_array_t<T> &array_d)
+{
+	return cv::gpu::GpuMat(array_d.size(0), array_d.size(1), cv::DataType<T>::type, array_d.raw_ptr());
+}
+
+template <typename T>
+inline const cv::gpu::GpuMat to_cv_gpu_mat(const device_array_t<T> &array_d)
+{
+	return cv::gpu::GpuMat(array_d.size(0), array_d.size(1), cv::DataType<T>::type, const_cast<T *>(array_d.raw_ptr()));
+}
+
+#endif // USE_CUDA
 
 inline array_t<uint8_t> imread(const std::string &filename)
 {
