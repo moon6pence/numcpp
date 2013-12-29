1d7d198d30b848fb128137a5ccd18ec8f2fa3460
khkim <moon6pence@gmail.com>
2013-12-30 01:26:22 +0900

    Update numcpp.cpp to include all numcpp headers and test compile

--- numcpp.cpp
+++ numcpp.cpp
@@ -1,2 +1,11 @@
-#include "numcpp/array.h"
-#include "numcpp/stl.h"
+#include "config.h"
+
+#include <numcpp/numcpp.h>
+
+#ifdef USE_OPENCV
+#include <numcpp/opencv.h>
+#endif
+
+#ifdef USE_CUDA
+#include <numcpp/cuda.h>
+#endif
