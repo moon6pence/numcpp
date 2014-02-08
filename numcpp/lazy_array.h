cce277d250ea2ad8f57f9cf770fd401c5b8557fd
khkim <moon6pence@gmail.com>
2014-02-08 11:49:50 +0900

    Add function array_cast<T>

--- numcpp/lazy_array.h
+++ numcpp/lazy_array.h
@@ -78,60 +78,6 @@ private:
 	T value;
 };
 
-template <class Array1, typename T, T Function(T)>
-struct lazy_array_with_unary_function
-{
-	typedef T element_type;
-
-	lazy_array_with_unary_function(const Array1 &a1) : a1(a1)
-	{
-	}
-
-	int ndims() const
-	{
-		return a1.ndims();
-	}
-
-	int *shape() const
-	{
-		return a1.shape();
-	}
-	
-	int size() const
-	{
-		return a1.size();
-	}
-
-	T at(int index0) const
-	{
-		return Function(a1.at(index0));
-	}
-
-private:
-	const Array1 &a1;
-};
-
-// template <typename T>
-// array_t<T> add(const array_t<T> &a1, const array_t<T> &a2)
-// {
-// 	// TODO: assert shape
-// 	assert(a1.size() == a2.size());
-
-// 	array_t<T> result(a1.ndims(), a1.shape());
-// 	transform(result, a1, a2, [](T _a1, T _a2) -> T { return _a1 + _a2; });
-// 	return result;
-// }
-
-// template <typename T>
-// void assign(array_t<T> &dst, const array_t<T> &src)
-// {
-// 	dst.setSize(src.ndims(), src.shape());
-
-// 	auto _src = begin(src);
-// 	for (auto _dst = begin(dst); _dst != end(dst); ++_dst, ++_src)
-// 		*_dst = *_src;
-// }
-
 // template <class Array1, class Array2>
 // lazy_array_with_binary_function<
 // 	Array1, Array2, typename Array1::element_type, _add> 
@@ -173,14 +119,54 @@ MAP_FUNC2(atan2, element_func::atan2)
 // 		Array1, typename Array1::element_type, _minus>(a1);
 // }
 
+template <class Array1, typename T, typename U, T Function(U)>
+struct lazy_array_with_unary_function
+{
+	typedef T element_type;
+
+	lazy_array_with_unary_function(const Array1 &a1) : a1(a1)
+	{
+	}
+
+	int ndims() const
+	{
+		return a1.ndims();
+	}
+
+	int *shape() const
+	{
+		return a1.shape();
+	}
+	
+	int size() const
+	{
+		return a1.size();
+	}
+
+	T at(int index0) const
+	{
+		return Function(a1.at(index0));
+	}
+
+private:
+	const Array1 &a1;
+};
+
 #define MAP_FUNC1(ARRAY_FUNC, ELEMENT_FUNC)\
 	template <class Array1>\
 	lazy_array_with_unary_function<\
-		Array1, typename Array1::element_type, ELEMENT_FUNC> \
+		Array1, \
+		decltype(ELEMENT_FUNC(typename Array1::element_type())), \
+		typename Array1::element_type, \
+		ELEMENT_FUNC> \
 		ARRAY_FUNC(const Array1 &a1)\
 	{\
 		return lazy_array_with_unary_function<\
-			Array1, typename Array1::element_type, ELEMENT_FUNC>(a1);\
+			Array1, \
+			decltype(ELEMENT_FUNC(typename Array1::element_type())), \
+			typename Array1::element_type, \
+			ELEMENT_FUNC>\
+			(a1);\
 	}
 
 namespace element_func
@@ -189,12 +175,33 @@ namespace element_func
 	template <typename T> T cos(T a) { return ::cos(a); }
 	template <typename T> T sin(T a) { return ::sin(a); }
 	template <typename T> T sqrt(T a) { return ::sqrt(a); }
+
+	template <typename T, typename U> T cast(U a) 
+	{ 
+		return static_cast<T>(a);
+	}
 }
 
 MAP_FUNC1(minus, element_func::minus)
 MAP_FUNC1(cos, element_func::cos)
 MAP_FUNC1(sin, element_func::sin)
 MAP_FUNC1(sqrt, element_func::sqrt)
+
+template <typename T, class Array1>
+lazy_array_with_unary_function<
+	Array1, 
+	T, 
+	typename Array1::element_type, 
+	element_func::cast<T, typename Array1::element_type>> 
+	array_cast(const Array1 &a1)
+{
+	return lazy_array_with_unary_function<
+		Array1, 
+		T, 
+		typename Array1::element_type, 
+		element_func::cast<T, typename Array1::element_type>> 
+		(a1);
+}
 
 // Wake up lazy array
 template <typename T, class LazyArray>
--- numcpp/lazy_array.test.inl
+++ numcpp/lazy_array.test.inl
@@ -3,14 +3,12 @@
 TEST(LazyArray, AddArrays)
 {
 	array_t<int> a1(5), a2(5), result;
-
 	a1(0) = 1; a1(1) = 2; a1(2) = 3; a1(3) = 4; a1(4) = 5;
 	a2(0) = 3; a2(1) = 3; a2(2) = 3; a2(3) = 3; a2(4) = 3;
 
 	assign(result, add(a1, a2));
 
 	ASSERT_EQ(5, result.size(0));
-
 	for (int i = 0; i < result.size(0); i++)
 		EXPECT_EQ(a1(i) + a2(i), result(i));
 }
@@ -18,7 +16,6 @@ TEST(LazyArray, AddArrays)
 TEST(LazyArray, AddConstantToArray)
 {
 	array_t<int> a1(5), result;
-
 	a1(0) = 1; a1(1) = 2; a1(2) = 3; a1(3) = 4; a1(4) = 5;
 
 	assign(result, add(a1, 3));
@@ -31,13 +28,11 @@ TEST(LazyArray, AddConstantToArray)
 TEST(LazyArray, InvertArraySign)
 {
 	array_t<int> a1(5), result;
-
 	a1(0) = 1; a1(1) = 2; a1(2) = 3; a1(3) = 4; a1(4) = 5;
 
 	assign(result, minus(a1));
 
 	ASSERT_EQ(5, result.size(0));
-
 	for (int i = 0; i < result.size(0); i++)
 		EXPECT_EQ(-a1(i), result(i));
 }
@@ -45,16 +40,27 @@ TEST(LazyArray, InvertArraySign)
 TEST(LazyArray, ArrayExpression)
 {
 	array_t<int> a1(5), a2(5), result;
-
 	a1(0) = 1; a1(1) = 2; a1(2) = 3; a1(3) = 4; a1(4) = 5;
 	a2(0) = 3; a2(1) = 3; a2(2) = 3; a2(3) = 3; a2(4) = 3;
 
 	assign(result, add(a1, minus(a2)));
 
 	ASSERT_EQ(5, result.size(0));
-
 	for (int i = 0; i < result.size(0); i++)
 		EXPECT_EQ(a1(i) - a2(i), result(i));
+}
+
+TEST(LazyArray, ArrayCast)
+{
+	array_t<float> a1(5);
+	array_t<int> result;
+	a1(0) = 1.5; a1(1) = 2.5; a1(2) = 3.5; a1(3) = 4.5; a1(4) = 5.5;
+
+	assign(result, array_cast<int>(a1));
+
+	ASSERT_EQ(5, result.size(0));
+	for (int i = 0; i < result.size(0); i++)
+		EXPECT_EQ((int)a1(i), result(i));
 }
 
 template <typename T>
