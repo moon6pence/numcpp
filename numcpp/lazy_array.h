2f8d2a83ad0d934f041388c053f6769c0ae04c77
khkim <moon6pence@gmail.com>
2014-02-08 12:11:05 +0900

    Simplify template. These complicated templates cannot be compiled with VS2012

--- numcpp/lazy_array.h
+++ numcpp/lazy_array.h
@@ -78,6 +78,60 @@ private:
 	T value;
 };
 
+template <class Array1, typename T, T Function(T)>
+struct lazy_array_with_unary_function
+{
+	typedef T element_type;
+
+	lazy_array_with_unary_function(const Array1 &a1) : a1(a1)
+	{
+	}
+
+	int ndims() const
+	{
+		return a1.ndims();
+	}
+
+	int *shape() const
+	{
+		return a1.shape();
+	}
+	
+	int size() const
+	{
+		return a1.size();
+	}
+
+	T at(int index0) const
+	{
+		return Function(a1.at(index0));
+	}
+
+private:
+	const Array1 &a1;
+};
+
+// template <typename T>
+// array_t<T> add(const array_t<T> &a1, const array_t<T> &a2)
+// {
+// 	// TODO: assert shape
+// 	assert(a1.size() == a2.size());
+
+// 	array_t<T> result(a1.ndims(), a1.shape());
+// 	transform(result, a1, a2, [](T _a1, T _a2) -> T { return _a1 + _a2; });
+// 	return result;
+// }
+
+// template <typename T>
+// void assign(array_t<T> &dst, const array_t<T> &src)
+// {
+// 	dst.setSize(src.ndims(), src.shape());
+
+// 	auto _src = begin(src);
+// 	for (auto _dst = begin(dst); _dst != end(dst); ++_dst, ++_src)
+// 		*_dst = *_src;
+// }
+
 // template <class Array1, class Array2>
 // lazy_array_with_binary_function<
 // 	Array1, Array2, typename Array1::element_type, _add> 
@@ -119,12 +173,35 @@ MAP_FUNC2(atan2, element_func::atan2)
 // 		Array1, typename Array1::element_type, _minus>(a1);
 // }
 
-template <class Array1, typename T, typename U, T Function(U)>
-struct lazy_array_with_unary_function
+#define MAP_FUNC1(ARRAY_FUNC, ELEMENT_FUNC)\
+	template <class Array1>\
+	lazy_array_with_unary_function<\
+		Array1, typename Array1::element_type, ELEMENT_FUNC> \
+		ARRAY_FUNC(const Array1 &a1)\
+	{\
+		return lazy_array_with_unary_function<\
+			Array1, typename Array1::element_type, ELEMENT_FUNC>(a1);\
+	}
+
+namespace element_func
+{
+	template <typename T> T minus(T a) { return -a; }
+	template <typename T> T cos(T a) { return ::cos(a); }
+	template <typename T> T sin(T a) { return ::sin(a); }
+	template <typename T> T sqrt(T a) { return ::sqrt(a); }
+}
+
+MAP_FUNC1(minus, element_func::minus)
+MAP_FUNC1(cos, element_func::cos)
+MAP_FUNC1(sin, element_func::sin)
+MAP_FUNC1(sqrt, element_func::sqrt)
+
+template <typename T, class Array1>
+struct lazy_array_cast
 {
 	typedef T element_type;
 
-	lazy_array_with_unary_function(const Array1 &a1) : a1(a1)
+	lazy_array_cast(const Array1 &a1) : a1(a1)
 	{
 	}
 
@@ -145,62 +222,17 @@ struct lazy_array_with_unary_function
 
 	T at(int index0) const
 	{
-		return Function(a1.at(index0));
+		return static_cast<T>(a1.at(index0));
 	}
 
 private:
 	const Array1 &a1;
 };
 
-#define MAP_FUNC1(ARRAY_FUNC, ELEMENT_FUNC)\
-	template <class Array1>\
-	lazy_array_with_unary_function<\
-		Array1, \
-		decltype(ELEMENT_FUNC(typename Array1::element_type())), \
-		typename Array1::element_type, \
-		ELEMENT_FUNC> \
-		ARRAY_FUNC(const Array1 &a1)\
-	{\
-		return lazy_array_with_unary_function<\
-			Array1, \
-			decltype(ELEMENT_FUNC(typename Array1::element_type())), \
-			typename Array1::element_type, \
-			ELEMENT_FUNC>\
-			(a1);\
-	}
-
-namespace element_func
-{
-	template <typename T> T minus(T a) { return -a; }
-	template <typename T> T cos(T a) { return ::cos(a); }
-	template <typename T> T sin(T a) { return ::sin(a); }
-	template <typename T> T sqrt(T a) { return ::sqrt(a); }
-
-	template <typename T, typename U> T cast(U a) 
-	{ 
-		return static_cast<T>(a);
-	}
-}
-
-MAP_FUNC1(minus, element_func::minus)
-MAP_FUNC1(cos, element_func::cos)
-MAP_FUNC1(sin, element_func::sin)
-MAP_FUNC1(sqrt, element_func::sqrt)
-
 template <typename T, class Array1>
-lazy_array_with_unary_function<
-	Array1, 
-	T, 
-	typename Array1::element_type, 
-	element_func::cast<T, typename Array1::element_type>> 
-	array_cast(const Array1 &a1)
+lazy_array_cast<T, Array1> array_cast(const Array1 &a1)
 {
-	return lazy_array_with_unary_function<
-		Array1, 
-		T, 
-		typename Array1::element_type, 
-		element_func::cast<T, typename Array1::element_type>> 
-		(a1);
+	return lazy_array_cast<T, Array1>(a1);
 }
 
 // Wake up lazy array
