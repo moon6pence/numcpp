b29bf13ada4645e78f08372182c2362c82d5b30f
khkim2 <moon6pence@gmail.com>
2014-10-20 13:41:27 +0900

    Link jsoncpp library in proper way

---
+++ jsoncpp/lib/cmake/jsoncpp/jsoncpp-config.cmake
@@ -0,0 +1,11 @@
+# Find the jsoncpp includes and library
+#
+# jsoncpp_INCLUDE_DIRS - where to find json/json.h
+# jsoncpp_LIBRARIES - list of libraries to link against.
+# jsoncpp_FOUND - set to 1 if found.
+
+set( jsoncpp_FOUND 1 )
+
+set( jsoncpp_INCLUDE_DIRS "C:/Users/khkim/build/jsoncpp-config-cmake.build/install/include" )
+
+set( jsoncpp_LIBRARIES "C:/Users/khkim/build/jsoncpp-config-cmake.build/install/lib/jsoncpp.lib" )
