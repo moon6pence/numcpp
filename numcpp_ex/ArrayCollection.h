5baa2cd7516a0edfaf476b6133854bbaf7a5cecc
khkim <moon6pence@gmail.com>
2016-01-11 18:46:10 +0900

    ArrayCollection: drop const functions

--- numcpp_ex/ArrayCollection.h
+++ numcpp_ex/ArrayCollection.h
@@ -7,8 +7,8 @@
 template <typename T, int Dim = 1>
 struct ArrayCollection
 {
-	virtual int size() const = 0;
-    virtual np::Array<T, Dim> at(int index) const = 0;
+	virtual int size() = 0;
+    virtual np::Array<T, Dim> at(int index) = 0;
 };
 
 #endif // ARRAY_COLLECTION_H_
