9abeb38c449faa8cc50815b7d238e51d5943809c
khkim <moon6pence@gmail.com>
2016-01-20 23:02:55 +0900

    Add ArrayVector, wrapper for std::vector<np::Array<T, Dim>>

--- numcpp_ex/ArrayCollection.h
+++ numcpp_ex/ArrayCollection.h
@@ -2,6 +2,7 @@
 #define ARRAY_COLLECTION_H_
 
 #include <numcpp/array.h>
+#include <vector>
 
 // ArrayCollection interface
 template <typename T, int Dim = 1>
@@ -9,6 +10,25 @@ struct ArrayCollection
 {
 	virtual int size() = 0;
     virtual np::Array<T, Dim> at(int index) = 0;
+};
+
+// wrapper for std::vector<np::Array<T, Dim>> to use as ArrayCollection<T, Dim>
+template <typename T, int Dim>
+class ArrayVector : public ArrayCollection<T, Dim>
+{
+public:
+    std::vector<np::Array<T, Dim>> arrays;
+
+    // implements ArrayCollection
+    int size() override 
+    { 
+        return (int)arrays.size(); 
+    }
+
+    np::Array<T, Dim> at(int index) override
+    {
+        return np::Array<T, Dim>(arrays.at(index));
+    }
 };
 
 #endif // ARRAY_COLLECTION_H_
