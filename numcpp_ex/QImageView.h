31fc2ddab471fbf78092bc14fd1e5d51597694d8
khkim <moon6pence@gmail.com>
2016-01-26 19:14:09 +0900

    QImageView.setInverted()

--- numcpp_ex/QImageView.h
+++ numcpp_ex/QImageView.h
@@ -9,14 +9,19 @@
 class QImageView : public QWidget
 {
 public:
-    QImageView(QWidget *parent = 0) : QWidget(parent), _colorTable(256)
+	QImageView(QWidget *parent = 0) : QWidget(parent), 
+		_standardColorTable(256),
+		_invertedColorTable(256)
     {
         // prepare color table
-        for (int i = 0; i < 256; i++)
-            _colorTable[i] = 0xFF000000 + i * 0x010101; 
+		for (int i = 0; i < 256; i++)
+			_standardColorTable[i] = 0xFF000000 + i * 0x010101;
+
+		for (int i = 0; i < 256; i++)
+			_invertedColorTable[i] = 0xFF000000 + (255 - i) * 0x010101; // Inverted color
 
         _qimage = QImage(256, 256, QImage::Format_Indexed8);
-        _qimage.setColorTable(_colorTable);
+        _qimage.setColorTable(_standardColorTable);
     }
 
     void setImage(const np::Array<uint8_t, 2> &image)
@@ -25,8 +30,8 @@ public:
         if (_qimage.width() != image.size(0) || _qimage.height() != image.size(1))
         {
             _qimage = QImage(image.size(0), image.size(1), QImage::Format_Indexed8);
-            _qimage.setColorTable(_colorTable);
-        }
+			_qimage.setColorTable(_inverted ? _invertedColorTable : _standardColorTable);
+		}
 
         // copy image data
         uchar *targetBuffer = _qimage.bits();
@@ -45,6 +50,17 @@ public:
 
         update();
     }
+
+	void setInverted(bool inverted)
+	{
+		_inverted = inverted;
+
+		if (!_qimage.isNull())
+		{
+			_qimage.setColorTable(_inverted ? _invertedColorTable : _standardColorTable);
+			update();
+		}
+	}
 
 	// signal
 	std::function<void(Qt::MouseButton button)> onClicked;
@@ -68,7 +84,8 @@ protected:
 
 private:
 	QImage _qimage;
-    QVector<QRgb> _colorTable;
+	QVector<QRgb> _standardColorTable, _invertedColorTable;
+	bool _inverted = false;
 	int _left = 0, _top = 0, _right = 0, _bottom = 0; // crop
 };
 
